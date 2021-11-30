e0a40642dd5b583a99b869f19daf58041fb6b128
Kvitka Mykola <nikolakvitka@gmail.com>
2021-11-30 14:13:35 +0200

    Добавьте файлы проекта.

---
+++ WebShop.DAL/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.DAL.Models
+{
+    public class Category
+    {
+        [Key]
+        public int NameId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Img { get; set; }
+
+        public List<Product> Product { get; set; }
+    }
+}
---
+++ WebShop.DAL/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebShop.DAL.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
